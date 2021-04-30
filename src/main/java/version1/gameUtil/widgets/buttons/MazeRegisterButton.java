a4f950e26f7b5ec523e63eb3cb32798c26f45c16
Sarai <sarai_alvarez86@hotmail.com>
2021-04-30 16:42:27 -0700

    Fixes to MazeRegisterButton and MazeTextField

--- src/main/java/version1/gameUtil/widgets/buttons/MazeRegisterButton.java
+++ src/main/java/version1/gameUtil/widgets/buttons/MazeRegisterButton.java
@@ -1,11 +1,11 @@
 package version1.gameUtil.widgets.buttons;
 
-import version1.gameUtil.listeners.interfaces.LoginBListenerIF;
+import version1.gameUtil.listeners.interfaces.RegisterBListenerIF;
 
 import java.awt.event.ActionListener;
 
 public class MazeRegisterButton extends MazeButton{
-    private LoginBListenerIF mediator;
+    private RegisterBListenerIF mediator;
     public MazeRegisterButton(String label, ActionListener listener) {
         super(label, listener);
     }
--- src/main/java/version1/gameUtil/widgets/inputfields/MazeTextField.java
+++ src/main/java/version1/gameUtil/widgets/inputfields/MazeTextField.java
@@ -1,10 +1,10 @@
 package version1.gameUtil.widgets.inputfields;
 
 import version1.gameUtil.listeners.implementations.AuthenticationManager;
-import version1.gameUtil.listeners.interfaces.LoginBListenerIF;
+import version1.gameUtil.listeners.interfaces.MazeTextFieldIF;
 
 public class MazeTextField extends AbstractMazeTextField {
-    private LoginBListenerIF mediator;
+    private MazeTextFieldIF mediator;
     public MazeTextField(String ph, AuthenticationManager listener){
 
     }
