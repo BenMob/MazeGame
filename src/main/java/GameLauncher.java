9ad762d00421bfb8e523e3c9874d0b2b28401d6d
Benjamin <omben001@cougars.csusm.edu>
2021-05-01 09:15:17 -0700

    implements GameLauncher

--- src/main/java/GameLauncher.java
+++ src/main/java/GameLauncher.java
@@ -2,6 +2,7 @@ import version1.gameUtil.GameFrame;
 
 public class GameLauncher {
     public static void main(String[] args) {
-        new GameFrame();        // Launche version1 of the MazeGame
+        GameFrame game = new GameFrame();
+        game.launch();
     }
 }
