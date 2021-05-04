c2ad3ce5c54bf7aac9bd7465c21778e28463fbf0
Benjamin <omben001@cougars.csusm.edu>
2021-05-04 09:53:37 -0700

    implements the MenuScreen

--- src/main/java/version1/gameUtil/screens/MenuScreen.java
+++ src/main/java/version1/gameUtil/screens/MenuScreen.java
@@ -4,9 +4,10 @@ import version1.gameUtil.GameFrame;
 import version1.gameUtil.settings.UIConfigurations;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuScreen extends AbstractScreen{
-    private JLabel header;
+
     private JButton playButton;
     private JButton leaderBoardButton;
     private JButton quitButton;
@@ -22,6 +23,27 @@ public class MenuScreen extends AbstractScreen{
     @Override
     public void buildHeader() {
 
+        /*
+         * First things first: sets the layout manager of the GameFrame
+         */
+        this.gameFrame.setLayout(new BorderLayout());
+
+        /*
+         * Initalize the header container
+         */
+        final JPanel headerContainer = new JPanel();
+
+        /*
+         * Style and Add header container
+         */
+        headerContainer.setBackground(gameFrame.getContentPane().getBackground());
+        headerContainer.setSize(new Dimension(gameFrame.getContentPane().getWidth(), gameFrame.getContentPane().getHeight() / 4));
+        headerContainer.add(this.headerLabel);
+
+        /*
+         * Lastly Add final result
+         */
+        this.add(headerContainer, BorderLayout.NORTH);
     }
 
     /**
@@ -29,6 +51,51 @@ public class MenuScreen extends AbstractScreen{
      */
     @Override
     public void buildBody() {
+        /*
+         * First Things First: Initliaze the
+         */
+        final ImageIcon backgroundImage = new ImageIcon("background.png");
+        final ImageIcon mazeGameLogoImage = new ImageIcon("mazegametitle.png");
+        final JLabel mazeGameLogo = new JLabel(mazeGameLogoImage);
+        final JLabel bodyContainer = new JLabel(backgroundImage);
+        final JPanel menuOptionsContainer = new JPanel();
+        final Font buttonFont = new Font("Bold", Font.BOLD, 20);
+        final int rows = 12;
+        final int columns = 1;
+
+
+        /*
+         * Define Layouts
+         */
+        mazeGameLogo.setLayout(new GridBagLayout());
+        bodyContainer.setLayout(new BorderLayout());
+        menuOptionsContainer.setLayout(new GridLayout(rows, columns));
+
+        /*
+         * Style Buttons
+         */
+        playButton.setFont(buttonFont);
+        leaderBoardButton.setFont(buttonFont);
+        quitButton.setFont(buttonFont);
+
+        /*
+         * Fill menu option container
+         */
+        menuOptionsContainer.add(playButton);
+        menuOptionsContainer.add(leaderBoardButton);
+        menuOptionsContainer.add(quitButton);
+        menuOptionsContainer.setOpaque(false);
+
+        /*
+         * Adds menu option container
+         */
+        mazeGameLogo.add(menuOptionsContainer);
+        bodyContainer.add(mazeGameLogo, BorderLayout.CENTER);
+
+        /*
+         * Display everything
+         */
+        this.add(bodyContainer, BorderLayout.CENTER);
 
     }
 
@@ -39,5 +106,36 @@ public class MenuScreen extends AbstractScreen{
     @Override
     public void ready() {
 
+    }
+
+    /**
+     * Creates the Button
+     * @param label the text label of the button
+     * @return this
+     */
+    public MenuScreen setPlayButtonText(String label) {
+        this.playButton = new JButton(label);
+        return this;
+    }
+
+    /**
+     * Creates the leaderboard button with the follwonf label
+     * @param label: a String
+     * @return this
+     */
+    public MenuScreen setLeaderBoardButton(String label) {
+        this.leaderBoardButton = new JButton(label);
+        return this;
+    }
+
+    /**
+     * Creates the quit button with the following label
+     * @param label A String
+     * @return this
+     */
+    public MenuScreen setQuitButtonText(String label) {
+        this.quitButton = new JButton(label);
+        this.quitButton.addActionListener(e -> this.gameFrame.goTo(new LoginScreen(gameFrame)));
+        return this;
     }
 }
